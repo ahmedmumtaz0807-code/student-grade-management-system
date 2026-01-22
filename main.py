d16ec64039d85b3fb21c90ad66bc13ff74c97134
ahmedmumtaz0807-code <ahmedmumtaz0807@gmail.com>
2026-01-21 20:33:52 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# step_counter()
+def step_counter(steps):
+    if steps >= 10000 :
+        print("enough steps for today")
+    else:
+        print("walk more")
+step_counter(int(input("Enter number of Steps:")))
+
+
